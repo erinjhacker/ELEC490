075a7890cb9fcee43726e18eae8fbf2328f1a2af
Erin Hacker <ehacker@limelightplatform.com>
2019-11-16 18:14:03 -0500

    Completely restarted. Now able to check permissions, request bt enable, and scan for devices. Next step is to list the devices found and allow user to choose one.

---
+++ app/src/main/java/com/example/elec490/ReadDevice.java
@@ -0,0 +1,19 @@
+package com.example.elec490;
+
+import android.content.Intent;
+
+public class ReadDevice {
+   /*
+    private static final int RESULT_OK = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == ) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+
+            }
+        }
+    } */
+}
