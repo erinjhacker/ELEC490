2dc12ffd7dc4e4ba4b0f1a288f3ac2b4977dfa4f
stevencrosby7 <stevencrosby7@icloud.com>
2019-11-21 15:15:04 -0500

    Remove bluetooth code to be able to run UI

--- app/src/main/java/com/example/elec490/MainActivity.java
+++ app/src/main/java/com/example/elec490/MainActivity.java
@@ -42,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
     HashSet<BluetoothDevice> devices = new HashSet<>();
     ArrayList<String> deviceNames = new ArrayList<>();
 
-    BluetoothDevice chosenDevice;
-
-    BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-    BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
+//    BluetoothDevice chosenDevice;
+//
+//    BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+//    BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
 
     private int REQUEST_ENABLE_BT = 1;
 
@@ -61,53 +61,53 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         //If bluetooth off, ask to turn on
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-
-        //Scan for device
-        if (scanner != null) {
-            scanner.startScan(scanCallback);
-            Log.d(TAG, "scan started");
-        }  else {
-            Log.e(TAG, "could not get scanner object");
-        }
+//        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        if (!bluetoothAdapter.isEnabled()) {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//        }
+//
+//        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+//            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
+//            finish();
+//            return;
+//        }
+//
+//        //Scan for device
+//        if (scanner != null) {
+//            scanner.startScan(scanCallback);
+//            Log.d(TAG, "scan started");
+//        }  else {
+//            Log.e(TAG, "could not get scanner object");
+//        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        final ListView simpleList = (ListView)findViewById(R.id.devices);
-        ArrayAdapter adapter = new ArrayAdapter<String>(
-                this, R.layout.activity_listview, R.id.textView, deviceNames);
-        simpleList.setAdapter(adapter);
-        simpleList.setClickable(true);
-        simpleList.setTextFilterEnabled(true);
-        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
-            String deviceName=(String) simpleList.getItemAtPosition(position);
-
-            for (BluetoothDevice device : devices) {
-                if (device.getName().equals(deviceName))
-                {
-                    chosenDevice = device;
-                }
-            }
-
-            goToReadDevice(chosenDevice);
-            }
-
-        });
+//        final ListView simpleList = (ListView)findViewById(R.id.devices);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(
+//                this, R.layout.activity_listview, R.id.textView, deviceNames);
+//        simpleList.setAdapter(adapter);
+//        simpleList.setClickable(true);
+//        simpleList.setTextFilterEnabled(true);
+//        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+//            String deviceName=(String) simpleList.getItemAtPosition(position);
+//
+//            for (BluetoothDevice device : devices) {
+//                if (device.getName().equals(deviceName))
+//                {
+//                    chosenDevice = device;
+//                }
+//            }
+//
+//            goToReadDevice(chosenDevice);
+//            }
+//
+//        });
     }
 
     protected void goToReadDevice(BluetoothDevice device) {
@@ -141,40 +141,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private final ScanCallback scanCallback = new ScanCallback() {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            BluetoothDevice device = result.getDevice();
-            //Will need to filter based on UUID (maybe also name and RSSI value)
-            //TODO: Should not have empty catch - find better way to implement
-            //TODO: Shouldn't be getting dupes because of ArrayList but we are - fio
-            //TODO: Add loading bar - not urgent but would look nice
-            try {
-                if (!device.getName().equals("null")) {
-                    devices.add(device);
-                }
-            } catch (Exception e){}
+//        @Override
+//        public void onScanResult(int callbackType, ScanResult result) {
+//            BluetoothDevice device = result.getDevice();
+//            //Will need to filter based on UUID (maybe also name and RSSI value)
+//            //TODO: Should not have empty catch - find better way to implement
+//            //TODO: Shouldn't be getting dupes because of ArrayList but we are - fio
+//            //TODO: Add loading bar - not urgent but would look nice
+//            try {
+//                if (!device.getName().equals("null")) {
+//                    devices.add(device);
+//                }
+//            } catch (Exception e){}
+//
+//            //TODO: Figure out why size not always set at 10 - not actually stopping or is at least restarting the scan
+//            //TODO: Change this in case 5 devices can't be found - don't want to get NPE
+//            if (devices.size() == 5) {
+//                scanner.stopScan(scanCallback);
+//                for (BluetoothDevice dev : devices) {
+//                    deviceNames.add(dev.getName());
+//                }
+//                //TODO: Create new method - don't use onResume - likely why there are dupes
+//                onResume();
+//            }
+//        }
 
-            //TODO: Figure out why size not always set at 10 - not actually stopping or is at least restarting the scan
-            //TODO: Change this in case 5 devices can't be found - don't want to get NPE
-            if (devices.size() == 5) {
-                scanner.stopScan(scanCallback);
-                for (BluetoothDevice dev : devices) {
-                    deviceNames.add(dev.getName());
-                }
-                //TODO: Create new method - don't use onResume - likely why there are dupes
-                onResume();
-            }
-        }
-
-        @Override
-        public void onBatchScanResults(List<ScanResult> results) {
-            // Ignore for now
-        }
-
-        @Override
-        public void onScanFailed(int errorCode) {
-            // Ignore for now
-        }
+//        @Override
+//        public void onBatchScanResults(List<ScanResult> results) {
+//            // Ignore for now
+//        }
+//
+//        @Override
+//        public void onScanFailed(int errorCode) {
+//            // Ignore for now
+//        }
 
     };
 }
